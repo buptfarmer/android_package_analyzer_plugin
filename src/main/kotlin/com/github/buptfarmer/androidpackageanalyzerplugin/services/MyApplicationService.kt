e7ebd3ab047ea5c11f7573eae653ca16eb96712f
GitHub Action <action@github.com>
2021-09-15 08:42:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/buptfarmer/androidpackageanalyzerplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.buptfarmer.androidpackageanalyzerplugin.services
+
+import com.github.buptfarmer.androidpackageanalyzerplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
